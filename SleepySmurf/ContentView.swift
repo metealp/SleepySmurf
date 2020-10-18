35c8a1c93abd2ca46aae9bca04a2cf897ef0d600
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-18 17:01:26 +0300

    Initial Commit

---
+++ SleepySmurf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SleepySmurf
+//
+//  Created by Mete Alp Kizilcay on 18.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepySmurf/SleepySmurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepySmurfApp.swift
+//  SleepySmurf
+//
+//  Created by Mete Alp Kizilcay on 18.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SleepySmurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
